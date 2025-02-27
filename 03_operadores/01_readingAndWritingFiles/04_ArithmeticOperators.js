0d2ecd546de600c1dc5fc40c7a3b53e3500fcbdd
AronJustoC <aronchoque727@gmail.com>
2025-02-27 09:29:48 -0500

    Arithmetic Operators

---
+++ 03_operadores/01_readingAndWritingFiles/04_ArithmeticOperators.js
@@ -0,0 +1,30 @@
+//01 Operadores aritméticos
+//Adición (+)
+let suma = 5 + 3;
+console.log('La suma de 5 y 3 es', suma);
+
+//Resta (-)
+let resta = 5 - 3;
+console.log('La resta de 5 y 3 es', resta);
+
+//Multiplicación (*)
+let multiplicacion = 5 * 3;
+console.log('La multiplicación de 5 y 3 es', multiplicacion);
+
+//División (/)
+let division = 5 / 3;
+console.log('La división de 5 y 3 es', division);
+
+//Modulo (%)
+let modulo = 5 % 3;
+console.log('El resto de 5 dividido entre 3 es', modulo);
+
+//Incremento
+let incremento = 5;
+incremento++;
+console.log('Incremento de 5 es', incremento);
+
+//Decremento
+let decremento = 5;
+decremento--;
+console.log('Decremento de 5 es', decremento);
